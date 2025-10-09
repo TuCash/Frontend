d18838676abc0c542d9c574b2fbb30e2ec55c26e
harrison <harrisonpayesa@gmail.com>
2025-10-09 13:51:30 -0500

    feat(incomes): add incomes module with service integration

---
+++ src/app/features/incomes/incomes.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { IncomesPageComponent } from './incomes-page.component';
+
+export const incomesRoutes: Routes = [
+  {
+    path: '',
+    component: IncomesPageComponent,
+  },
+];
